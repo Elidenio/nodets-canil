25366cdb3301fdd1516ed055aed135dd5514c6ca
Elidenio <127259082+Elidenio@users.noreply.github.com>
2025-08-06 21:07:46 -0300

    Final project

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject'
+import { pet } from '../models/pet'
 
  export const search = (req: Request, res: Response) =>{
-    //res.send('pages/search')
+    let query: string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/')
+        return;
+    }
+
+    let list = pet.getFromName(query)
+    
+    res.render('pages/page',{
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
